2d16ff41717580fb13c4d47889344a77c203dab7
Nikhil Bhavikatti <nikhilbhavikatti19@gmail.com>
2019-01-09 22:43:24 +0530

    Add Taking input of parameters from user

--- Decision_Making.py
+++ Decision_Making.py
@@ -1,5 +1,7 @@
 from sklearn import tree 
 
+values = []
+
 #[height,weight,shoe size]
 X = [[181,80,40], [177,70,43], [160,60,38], [154,54,37],
      [186,65,40], [190,90,47], [175,64,39], [177,70,40],
@@ -7,11 +9,16 @@ X = [[181,80,40], [177,70,43], [160,60,38], [154,54,37],
 
 Y = ['male','female','female','female','male','male','male','female','male','female','male']
 
+print('Enter height, weight and shoe size : ')
+for i in range(3) :
+	val = input()
+	values.append(int(val))
+
 clf = tree.DecisionTreeClassifier()
 
 #fit method trains decision tree on data set
 clf = clf.fit(X,Y)
 
-prediction = clf.predict([[190,70,43]])
+prediction = clf.predict([values])
 print(prediction)
 
